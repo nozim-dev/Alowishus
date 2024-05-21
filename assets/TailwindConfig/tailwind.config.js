514f7828334b679bfa6e530e93975efab252fc0a
nozim-dev <nozimfordevelop@gmail.com>
2024-05-21 17:17:25 +0500

    second section responsive done

--- assets/TailwindConfig/tailwind.config.js
+++ assets/TailwindConfig/tailwind.config.js
@@ -2,8 +2,10 @@ tailwind.config = {
     theme: {
         screens: {
             lg1240: { 'max': '1240px' },
+            lg1024: { 'max': '1024px' },
             lg1010: { 'max': '1010px' },
             lg910: { 'max': "910px" },
+            md820: { 'max': "820px" },
             md874: { 'max': "874px" },
             md750: { 'max': "750px" },
             md670: { 'max': "670px" },
